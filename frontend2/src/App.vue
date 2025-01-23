f2b3dfd8c8233082fe5b52ded24e02ba85fc6093
ale <alessioberti@gmail.com>
2025-01-23 15:22:45 +0100

    ok

---
+++ frontend2/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+  header {
+    background-color: #333;
+    color: white;
+    padding: 10px;
+    text-align: center;
+  }
+</style>
